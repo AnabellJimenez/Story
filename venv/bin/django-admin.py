4b131615d0c8a3dbd85b1e7470725d745efd6c95
Anabell <gonzalez.anabell@gmail.com>
2015-05-26 14:36:05 -0600

    heroku

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/anabell/Desktop/GoCode/gc-staging/GC_Story/story/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
