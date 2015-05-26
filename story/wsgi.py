05d32331a73618440cfa4c1bdcf6d777d9975dc3
Anabell <gonzalez.anabell@gmail.com>
2015-05-26 15:11:24 -0600

    testing

--- story/wsgi.py
+++ story/wsgi.py
@@ -14,6 +14,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "story.settings")
 
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
 application = DjangoWhiteNoise(application)
 
